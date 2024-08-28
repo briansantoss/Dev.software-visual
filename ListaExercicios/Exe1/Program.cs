dde4c841dcca1417381ddecb791ef120f6db7b92
Brian Santos <brian.freitas.santos@gmail.com>
2024-08-28 09:39:19 -0300

    Adicionando exercícios

---
+++ ListaExercicios/Exe1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Informe a altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a largura: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"A area total e de: {altura * largura}");
+
