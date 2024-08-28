a4842c6db355eca23b858a35aec5be0e6cb14e74
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-08-28 11:27:46 -0300

    Aprimorando a API

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Funcionalidade 1");
+app.MapGet("/segundafunc", () => "Funcionalidade 2");
+app.MapGet("/retornarendereco", () => {
+    dynamic endereco = new {
+        cep = "82100-741",
+        rua = "Parigot De Souza",
+        numero = 5300
+    };
+    return endereco;
+} );
 
+
+// Criar um endpoint pare receber informações
+// - 
 app.Run();
