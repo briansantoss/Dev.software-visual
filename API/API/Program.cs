7be946dd1e663f1cc0487cef88766d2f32519855
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-04 11:04:42 -0300

    Implementando funcionalidades de e-commerce

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,18 +1,36 @@
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Funcionalidade 1");
-app.MapGet("/segundafunc", () => "Funcionalidade 2");
-app.MapGet("/retornarendereco", () => {
-    dynamic endereco = new {
-        cep = "82100-741",
-        rua = "Parigot De Souza",
-        numero = 5300
-    };
-    return endereco;
-} );
+app.MapGet("/", () => "API de Produtos");
 
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto()
+{
+    Nome = "Notebook",
+    Preco = 5000,
+    Quantidade = 54
+});
 
-// Criar um endpoint pare receber informações
-// - 
+produtos.Add(new Produto() 
+{
+    Nome = "Mouse",
+    Preco = 100,
+    Quantidade = 120
+});
+
+produtos.Add(new Produto()
+{
+    Nome = "Teclado",
+    Preco = 450.45,
+    Quantidade = 24
+});
+
+app.MapGet("/produto/listar", () => 
+{
+    return produtos;
+});
+
+// Criar uma funcionalidade para receber informação
 app.Run();
