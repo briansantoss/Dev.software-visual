d6f44cd562b1a017595ef308dfc6535bb37950f4
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-25 11:38:09 -0300

    Configurando e implementando o DB

---
+++ API/API/Models/AppDataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+// Implementando herança da classe DbContext
+public class AppDataContext : DbContext
+{
+    // Definindo classes que representarão tabelas
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=ecommerce.db");
+    }
+}
